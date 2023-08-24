0ba71a7329710d6c7fa2fdb060fcadf214c53790
Isaac <59361485+rojamdev@users.noreply.github.com>
2023-08-24 21:18:15 +0100

    Fixes

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ import wget
 manifest = open("manifest.json")
 data = json.load(manifest)
 
-os.mkdir("mods")
+if not os.path.isdir("mods"):
+    os.mkdir("mods")
 
 blacklist = []
+skip_count = 0
 
 with open("blacklist.txt") as bl_file:
     for line in bl_file:
+        line = line.strip()
         blacklist.append(line)
-
-print(blacklist)
 
 for i in data["files"]:
     projectID = str(i["projectID"])
     fileID = str(i["fileID"])
+
+    if projectID in blacklist:
+        print("Skipping mod with project ID " + projectID)
+        skip_count += 1
+        continue
 
     url = "https://www.curseforge.com/api/v1/mods/" + projectID + "/files/" + fileID + "/download"
     filename = projectID + "_" + fileID + ".jar"
     output = os.path.join("mods", filename)
 
     wget.download(url, out=output)
+
+print("Download complete, " + str(skip_count) + " mods skipped.")
